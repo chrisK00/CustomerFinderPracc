c221381c061befdb9520727b706979573d2b778a
chris <christianhunter707@gmail.com>
2021-03-21 19:15:38 +0100

    customer print

--- CustomerManager-Client/src/app/_services/customer.service.ts
+++ CustomerManager-Client/src/app/_services/customer.service.ts
@@ -1,12 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Customer } from '../_interfaces/customer';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-
+  baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
+  getCustomers() {
+    return this.http.get<Customer[]>(this.baseUrl + 'customers');
+  }
 }
--- CustomerManager-Client/src/app/components/home/home.component.ts
+++ CustomerManager-Client/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/_interfaces/customer';
+import { CustomerService } from 'src/app/_services/customer.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  customers: Customer[];
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
-
+    this.loadCustomers();
   }
 
+  loadCustomers() {
+    this.customerService.getCustomers().subscribe(customers => this.customers = customers), error =>
+      console.log(error);
+  }
 
 }
