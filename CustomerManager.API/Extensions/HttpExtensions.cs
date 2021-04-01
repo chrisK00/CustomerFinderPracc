bec22957e341754824d581a3b638b6660a7e3e1a
chris <christianhunter707@gmail.com>
2021-04-01 14:45:10 +0200

    paged list class created and helpers setup

---
+++ CustomerManager.API/Extensions/HttpExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManager.API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader()
+        {
+
+        }
+    }
+}
---
+++ CustomerManager.API/Helpers/PagedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManager.API.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items,int pageNumber, int pageSize, int count)
+        {
+            CurrentPage = pageNumber;
+            PageSize = pageSize;
+            //want all the items to fit the pages
+            TotalPages = (int)Math.Ceiling(count/(double)PageSize);
+            TotalCount = count;
+            //add the items to the list
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// Executes a query and returns a paged list
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            //singleton query            
+            var count = await source.CountAsync();
+
+            //skip last page using the current pagenum - 1 * how many items every page is
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, pageNumber, pageSize, count);
+        }
+    }
+}
